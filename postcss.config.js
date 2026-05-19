d49174c3e288f944c144da353e6fceb6d4239161
zarimixtapes <69781222+zarimixtapes@users.noreply.github.com>
2026-05-19 10:40:46 +1000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,11 @@
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } };
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
